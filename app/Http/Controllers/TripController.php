4f647315e8e7b358c1107bfcaec02d1dce1643a9
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-04-15 15:10:52 +0600

    Adding websockets events

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\TripAccepted;
+use App\Events\TripEnded;
+use App\Events\TripLocationUpdated;
+use App\Events\TripStarted;
 use App\Models\Trip;
 use Illuminate\Http\Request;
 
@@ -50,6 +54,8 @@ class TripController extends Controller
 
         $trip->load('driver.user');
 
+        TripAccepted::dispatch($trip, $request->user());
+
         return $trip;
     }
 
@@ -62,6 +68,8 @@ class TripController extends Controller
 
         $trip->load('driver.user');
 
+        TripStarted::dispatch($trip, $request->user());
+
         return $trip;
     }
 
@@ -73,6 +81,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripEnded::dispatch($trip, $request->user());
 
         return $trip;
     }
@@ -89,6 +99,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripLocationUpdated::dispatch($trip, $request->user());
 
         return $trip;
     }
