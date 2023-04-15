93aec2d381372aae92fea3ad981f19131f15a02f
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-04-15 14:13:50 +0600

    Building the api

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -29,9 +25,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'login_code',
         'remember_token',
     ];
+
+    public function routeNotificationForTwilio()
+    {
+        return $this->phone;
+    }
 
     public function driver()
     {
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,26 @@
 <?php
 
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TripController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('/login', [LoginController::class, 'submit']);
+Route::post('/login/verify', [LoginController::class, 'verify']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function() {
+    Route::get('/driver', [DriverController::class, 'show']);
+    Route::post('/driver', [DriverController::class, 'update']);
+
+    Route::post('/trip', [TripController::class, 'store']);
+    Route::get('/trip/{trip}', [TripController::class, 'show']);
+    Route::get('/trip/{trip}/accept', [TripController::class, 'accept']);
+    Route::get('/trip/{trip}/start', [TripController::class, 'start']);
+    Route::get('/trip/{trip}/end', [TripController::class, 'end']);
+    Route::get('/trip/{trip}/location', [TripController::class, 'location']);
+
+    Route::get('/user', function(Request $request) {
+        return $request->user();
+    });
 });
