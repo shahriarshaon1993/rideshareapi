8fe45c2534dc7e6396bfb775152615de00016c1d
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-04-29 16:16:34 +0600

    Passenger flow

---
+++ frontend/src/views/TripView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="pt-16">
+        <h1 class="text-3xl font-semibold mb-4">{{ title }}</h1>
+        <div>
+            <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+                <div class="bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <GMapMap :zoom="14" ref="gMap"
+                            style="width:100%; height: 256px;">
+                        </GMapMap>
+                    </div>
+                </div>
+                <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <span>Messaage here</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
